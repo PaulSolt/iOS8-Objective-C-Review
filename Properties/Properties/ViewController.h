d7ec9819f56ca1a2190e3d41e134dabfe997bbc5
Paul Solt <PaulSolt@gmail.com>
2019-10-17 12:19:16 -0400

    Added

---
+++ Properties/Properties/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Properties
+//
+//  Created by Paul Solt on 10/17/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
